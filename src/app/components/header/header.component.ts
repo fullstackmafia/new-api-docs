82d7fc2a3d5f96ea797a154c7f6617b0c9888847
Farouk Bakre <faroukbakre@yahoo.com>
2022-06-28 16:52:56 +0100

    Fix: make pages responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    openMenu: boolean = false;
+    constructor(
+        private location: Location
+    ) {
+        this.location.onUrlChange(url => {
+            this.openMenu = false;
+            window.scrollTo(0, 0)
+        });
+    }
 
-  constructor() { }
+    ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
+    }
+
+    ngOnChanges() {
+
+        this.openMenu = false;
+    }
 
 }
